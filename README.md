caf8928399a5b5a50945aaa620618d8e449184f7
Masfikul Alam <masfikalamfrp@gmail.com>
2022-05-09 02:02:59 +0600

    updated UI

--- README.md
+++ README.md
@@ -2,12 +2,10 @@
 
 This is a website made for searching lyrics.
 
-* There is a searchbar where user can input song name
-* Matching songs are displayed below the searchbar.
-* Users can get lyrics from matching songs by clicking 'Get Lyrics'
-* Available lyrics would appear on it.
+- Search by music title, album or artist
+- Get lyrics from matching search results
 
-Technologies : HTML, CSS flexbox, JavaScript, API
+Technologies : HTML, CSS, JavaScript, API
 
-* [Live Site](https://masfikalam.github.io/Hard-Rock)
-* [Github](https://github.com/masfikalam/Hard-Rock)
+- [Live Site](https://masfikalam.github.io/Hard-Rock)
+- [Github](https://github.com/masfikalam/Hard-Rock)
--- script.js
+++ script.js
@@ -1,66 +1,56 @@
-// elements
-const searchInput = document.getElementById('search');
-const resultCount = document.getElementById('count');
-const searchResult = document.querySelector('.search-result .row');
+// dom elements
+const input = document.querySelector("input");
+const results = document.querySelector("main");
 
-// search song
-function searchSong() {
+// search music
+function searchMusic() {
+  results.innerHTML = "<h3>Loading...</h3>";
 
-    // fetching song details
-    fetch(`https://api.lyrics.ovh/suggest/${searchInput.value}`)
-        .then(response => response.json())
-        .then(info => {
-            if (searchResult.children.length == 10) {
-                searchResult.innerHTML = '';
-            }
-            for (let i = 0; i < 10; i++) {
-                const song = info.data[i];
-                displayDetails(info.total, song);
-            }
-        });
-    searchInput.value = '';
+  fetch(`https://api.lyrics.ovh/suggest/${input.value}`)
+    .then((response) => response.json())
+    .then(({ data }) => {
+      results.innerHTML = "";
+      data.forEach((music) => displayDetails(music));
+    })
+    .catch(() => {
+      results.innerHTML = "<h3>Something went wrong!</h3>";
+    });
 }
 
-// displaying song details
-function displayDetails(total, song) {
-    const div = document.createElement('div');
+// music details
+function displayDetails(music) {
+  const div = document.createElement("div");
+  div.setAttribute("id", `music-${music.id}`);
+  div.classList.add("card");
+  div.innerHTML = `
+  <div class="details">
+    <div>
+      <img src="${music.album.cover_medium}" />
+    </div>
 
-    // displaying total result
-    resultCount.innerText = `${total} songs found`;
+    <div>
+      <h3>${music.title}</h3>
+      <p>Album : ${music.album.title}</p>
+      <p>Artist : ${music.artist.name}</p>
+      <button onclick="displayLyrics(this, '${music.artist.name}', '${music.title}', '${music.id}')">Lyrics</button>
+    </div>
+  </div>`;
+  results.appendChild(div);
+}
 
-    // displaying song title, album, artist, photo
-    div.classList.add('col-lg-6');
-    div.innerHTML =
-        `<div id="song-${song.id}" class="single-result row justify-content-center align-items-center text-center m-3 p-3">
-            <div class="col-md-12">
-                <h3 class="mb-3">${song.title}</h3>
-                <p class="lead">Duration : ${Math.floor(song.duration/60)}:${song.duration%60}</p>
-                <p class="lead">Album : ${song.album.title}</p>
-                <p class="lead">Artist : ${song.artist.name}</p>
-                <img src="${song.artist.picture}" class="rounded-circle mb-3">
-            </div>
-            <div class="col-md-12 text-center">
-                <button id="button-${song.id}" class="btn btn-success">Get Lyrics</button>
-            </div>
-        </div>`;
-    searchResult.appendChild(div);
+// music lyrics
+function displayLyrics(target, artist, title, id) {
+  fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then((response) => response.json())
+    .then((info) => {
+      const para = document.createElement("p");
+      const horz = document.createElement("hr");
+      para.style.color = "#ddd0ff";
+      const card = results.querySelector(`#music-${id}`);
+      para.innerText = info.lyrics ? info.lyrics : "Lyrics not found!";
 
-    // get lyrics button
-    document.getElementById(`button-${song.id}`).addEventListener('click', () => {
-
-        // fetching song lyrics
-        fetch(`https://api.lyrics.ovh/v1/${song.artist.name}/${song.title}`)
-            .then(response => response.json())
-            .then(info => {
-                const lyricsResult = document.getElementById(`song-${song.id}`);
-                lyricsResult.classList.add('lyricsResult');
-
-                // checking if we have that lyrics
-                if (info.error) {
-                    lyricsResult.innerHTML = "Sorry, our API doesn't have this lyrics at this moment";
-                } else {
-                    lyricsResult.innerHTML = `<pre class="text-white">${info.lyrics}</pre>`;
-                };
-            });
+      target.style.display = "none";
+      card.appendChild(horz);
+      card.appendChild(para);
     });
 }
